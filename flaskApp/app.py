0df14d9933025c227298c6c5d3100ecbc89fb9f8
serhanayberkkilic <ayberk.kilic@peakup.org>
2023-08-29 23:14:32 +0300

    added first

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,6 +1,10 @@
 import sys
+from flaskApp.backend import index
 from flaskApp.backend.config import app
 
 sys.path.append('backend')
 
-if __name__ == '__main__': app.run(host="localhost", port=8000)
+if __name__ == '__main__': 
+    app.run(host="localhost", port=8000)
+
+
--- flaskApp/backend/index.py
+++ flaskApp/backend/index.py
@@ -1,6 +1,12 @@
-from config import app
-from flask import Flask, render_template
+from flaskApp.backend.config import app
+from flask import render_template
+
+
 @app.route('/')
-@app.route('/index')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
